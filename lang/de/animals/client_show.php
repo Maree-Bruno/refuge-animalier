03cbfadbc49cf45652a3fcc0042814c55f98bc2a
Bruno Marée <bruno.maree@student.hepl.be>
2025-11-30 14:35:10 +0100

    Add translations to animal show page

---
+++ lang/de/animals/client_show.php
@@ -0,0 +1,56 @@
+<?php
+return [
+    'title' => 'Billy',
+
+    'features_title' => 'Eigenschaften',
+    'features' => [
+        'age' => 'Alter',
+        'race' => 'Rasse',
+        'species' => 'Art',
+        'coat' => 'Fell',
+
+        'age_value' => '1 Jahr',
+        'race_value' => 'Hund',
+        'species_value' => 'Golden Retriever',
+        'coat_value' => 'Beige',
+
+        'fits_for' => 'Geeignet für',
+        'fits' => [
+            'dog' => 'Hund',
+            'cat' => 'Katze',
+            'baby' => 'Baby',
+            'child' => 'Kind',
+        ],
+
+        'walk' => 'Spaziergänge',
+        'walk_text' => 'Spaziergänge sind kein Problem',
+    ],
+
+    'description_title' => 'Beschreibung',
+    'description' => "Billy ist ein sanfter, liebevoller und energiegeladener Hund. Er verbringt gerne Zeit mit Menschen und freut
+        sich immer aufs Spielen oder Spazierengehen. Sehr sozial, versteht er sich gut mit anderen Hunden und hat keine Probleme,
+        neue Leute kennenzulernen.
+
+        Er lernt schnell, besonders mit positiver Erziehung. Billy liebt Outdoor-Aktivitäten: rennen, schnüffeln, erkunden… alles
+        interessiert ihn!
+
+        Trotz seiner Energie kann er sich auch gut entspannen und ruhige Momente bei vertrauten Menschen genießen. Billy passt perfekt
+        zu einer aktiven Familie oder jemandem, der gerne draußen ist. Mit Geduld, Freundlichkeit und einer stabilen Umgebung wird er
+        ein treuer und ausgeglichener Begleiter.",
+
+    'interested_title' => 'Interessiert?',
+
+    'adoption_text' => "Billy zu adoptieren bedeutet, einem Hund eine zweite Chance zu geben, der nur lieben und geliebt werden möchte.
+        Unser Team steht bereit, um alle Ihre Fragen zu beantworten und Sie durch den Adoptionsprozess zu begleiten.
+
+        Bevor Sie Ihre Anfrage senden, stellen Sie sicher, dass Sie ihm bieten können:
+        - Zeit für Spaziergänge und Aktivitäten,
+        - eine sichere Umgebung,
+        - Aufmerksamkeit und Stabilität,
+        - viel Zuneigung und Respekt.
+
+        Wenn Sie glauben, die richtige Familie für ihn zu sein, füllen Sie das Formular aus: Wir kontaktieren Sie schnell, um über Ihr
+        Adoptionsvorhaben zu sprechen und ein Treffen zu organisieren.",
+
+    'others_title' => 'Weitere Tiere, die Sie interessieren könnten',
+];
