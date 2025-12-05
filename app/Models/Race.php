231bbcb1b9a71583223df12d00e3d26937fc4c47
Bruno Marée <bruno.maree@student.hepl.be>
2025-12-05 16:15:13 +0100

    Add Specie model and species table migration

--- app/Models/Race.php
+++ app/Models/Race.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Race extends Model
 {
@@ -13,4 +14,10 @@ class Race extends Model
             'name' => 'array',
         ];
     }
+
+    public function specie(): HasOne
+    {
+        return $this->hasOne(Specie::class);
+    }
+
 }
---
+++ database/migrations/2025_12_05_150546_create_species_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('species', function (Blueprint $table) {
+            $table->id();
+            $table->json('name');
+            $table->foreignId('race_id')->constrained('races')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('species');
+    }
+};
