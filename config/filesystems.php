f7a4f9070f3099ebb77840b880c232f9c33d2730
Bruno Marée <bruno.maree@student.hepl.be>
2026-01-02 15:16:17 +0100

    Add AWS S3 support for image storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,20 +60,31 @@ return [
             'report' => false,
         ],
         'images' => [
-            'driver' => 'local',
+            'driver' => env('FILESYSTEM_DISK') === 's3' ? 's3' : 'local',
             'root' => storage_path('app/public/images'),
-            'url' => env('APP_URL').'/images',
+            'url' => env('FILESYSTEM_DISK') === 's3'
+                ? env('AWS_URL').'/images'
+                : env('APP_URL').'/storage/images',
             'visibility' => 'public',
             'throw' => false,
-            'report' => false,
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
         ],
+
         'userimages' => [
-            'driver' => 'local',
+            'driver' => env('FILESYSTEM_DISK') === 's3' ? 's3' : 'local',
             'root' => storage_path('app/public/images'),
-            'url' => env('APP_URL').'/images',
+            'url' => env('FILESYSTEM_DISK') === 's3'
+                ? env('AWS_URL').'/images'
+                : env('APP_URL').'/storage/images',
             'visibility' => 'public',
             'throw' => false,
-            'report' => false,
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
         ],
 
 
