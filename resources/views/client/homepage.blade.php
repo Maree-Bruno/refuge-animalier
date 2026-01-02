415610d4f64499dd6a66d6f206961d16c3a1e4ff
Bruno Marée <bruno.maree@student.hepl.be>
2026-01-02 15:43:07 +0100

    Update storage link and image asset references
    Restores the default public storage symlink in filesystems.php and replaces URL() with asset() for image paths in the homepage Blade view to ensure proper asset loading.

--- resources/views/client/homepage.blade.php
+++ resources/views/client/homepage.blade.php
@@ -42,7 +42,7 @@
                 <div class="relative group-img-homepage-1 place-self-end">
                     <div class="rounded-3xl overflow-hidden shadow-[0px_4px_30px_0px_rgba(0,0,0,0.25)]">
                         <img
-                            src="{{ URL('images/hamster.webp') }}"
+                            src="{{ asset('images/hamster.webp') }}"
                             alt="Hamster"
                             class="w-full h-full object-cover aspect-square lg:w-60 lg:h-auto"
                         />
@@ -55,7 +55,7 @@
                 <div class="relative group-img-homepage-2 place-self-start">
                     <div class="rounded-3xl overflow-hidden shadow-[0px_4px_30px_0px_rgba(0,0,0,0.25)]">
                         <img
-                            src="{{ URL('images/cat.webp') }}"
+                            src="{{ asset('images/cat.webp') }}"
                             alt="Chat"
                             class="w-full h-full object-cover aspect-square lg:w-60 lg:h-auto"
                         />
@@ -68,7 +68,7 @@
                 <div class="relative group-img-homepage-3">
                     <div class="rounded-3xl overflow-hidden shadow-[0px_4px_30px_0px_rgba(0,0,0,0.25)]">
                         <img
-                            src="{{ URL('images/billy.webp') }}"
+                            src="{{ asset('images/billy.webp') }}"
                             alt="Chien"
                             class="w-full h-full object-cover aspect-square lg:w-60 lg:h-auto"
                         />
