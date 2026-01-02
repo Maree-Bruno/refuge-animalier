29cd45ec2ea794eaa84eea7bc0f61870d1521169
Bruno Marée <bruno.maree@student.hepl.be>
2026-01-02 15:50:48 +0100

    Change 'name' columns from JSON to string in migrations

--- database/migrations/2025_12_05_145450_create_coats_table.php
+++ database/migrations/2025_12_05_145450_create_coats_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     {
         Schema::create('coats', function (Blueprint $table) {
             $table->id();
-            $table->json('name');
+            $table->string('name');
             $table->timestamps();
         });
     }
--- database/migrations/2025_12_05_150126_create_races_table.php
+++ database/migrations/2025_12_05_150126_create_races_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     {
         Schema::create('races', function (Blueprint $table) {
             $table->id();
-            $table->json('name');
+            $table->string('name');
             $table->foreignId('specie_id')->constrained('species')->cascadeOnDelete();
             $table->timestamps();
         });
